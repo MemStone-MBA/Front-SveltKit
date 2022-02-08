437f1d09b2f28c2d01551efe7d4f6606d9ecbf3b
GuillianV <guillianvibert90@gmail.com>
2022-02-08 16:30:41 +0100

    prepare landing

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,7 +4,7 @@
 <link rel='stylesheet' href='static/css/style.css'>
 
 
-<Nav/>
+<!--<Nav/>-->
 <!-- Slot représente le reste du code chragé par la page -->
 <slot></slot>
 
@@ -17,5 +17,52 @@
     @font-face {
     font-family: Hamston;
     src: url("../../static/font/Hamston.ttf");
+    }
+
+    @font-face {
+        font-family: "barlow-bold";
+        src: url("../../static/font/Barlow-Bold.ttf")
+    }
+
+    @font-face {
+        font-family: "barlow-medium";
+        src: url("../../static/font/Barlow-Medium.ttf")
+    }
+
+    @font-face {
+        font-family: "barlow-regular";
+        src: url("../../static/font/Barlow-Regular.ttf")
+    }
+
+    @font-face {
+        font-family: "barlow-light";
+        src: url("../../static/font/Barlow-Light.ttf")
+    }
+
+
+    @font-face {
+        font-family: "barlow-li";
+        src: url("../../static/font/Barlow-LightItalic.ttf")
+    }
+
+    @font-face {
+        font-family: "barlow-sb";
+        src: url("../../static/font/Barlow-SemiBold.ttf")
+    }
+
+
+    @font-face {
+        font-family: "barlow-sbi";
+        src: url("../../static/font/Barlow-SemiBoldItalic.ttf")
+    }
+
+    @font-face {
+        font-family: "barlow-thin";
+        src: url("../../static/font/Barlow-Thin.ttf")
+    }
+
+    @font-face {
+        font-family: "barlow-eb";
+        src: url("../../static/font/Barlow-ExtraBold.ttf")
     }
 </style>
--- src/routes/landing.svelte
+++ src/routes/landing.svelte
@@ -1,20 +1,100 @@
-<script>
-</script>
-
-<h1>
-	Un Aventure Mem-Orable
-</h1>
-
-hello landing
-
-<img src='static/assets/memstone_logo.png' class='img-contain'>
+	<script>
+	</script>
 
 
-<style>
+		<div class='header-landing w-full leading-10 flex'>
+			<div class='w-1/4 p-4 '>
+				<a href='/'><img src='static/assets/memstone_logo.png' class='img-contain'></a>
+
+			</div>
+			<div class=' w-full flex justify-end items-center pr-8'>
+				<div class='uppercase p-4 text-white cursor-pointer'>
+					Actualités
+				</div>
+				<div class='uppercase p-4 pr-16 pl-16 text-white cursor-pointer'>
+					Mon Compte
+				</div>
+				<div class='title-btn uppercase pr-16 pl-16 p-1 text-black bg-amber-300 cursor-pointer'>
+					Jouer
+				</div>
+			</div >
+		</div>
+
+	<div class='content-landing flex justify-center items-center w-full'>
+		<div class='content-landing-left grow-[2] w-3/5 h-full flex justify-center items-center'>
+			<div class='p-8'>
+				<h1 class='uppercase text-white '>
+					Memstone
+				</h1>
+				<h2 class='uppercase text-white '>
+					Un Aventure Mem-Orable
+				</h2>
+			</div>
+
+		</div>
+		<div class='content-landing-right grow-1 w-2/5 h-full flex justify-center bg-white items-center'>
+			<h3 class='uppercase p-4 cursor-pointer'>
+				Découvrir
+			</h3>
+		</div>
+	</div>
+ <div class='clear w-full h-32 bg-black'>
+
+ </div>
+
+	<div class='content-landing flex justify-center items-center w-full'>
+		<div class='content-landing-left grow-1 w-2/5 h-full flex justify-center bg-white items-center'>
+			<div class='p-8'>
+				<h3 class='uppercase p-4 cursor-pointer'>
+					Découvrir
+				</h3>
+
+			</div>
+
+		</div>
+		<div class='content-landing-right grow-[2]  w-3/5  h-full flex justify-center bg-black items-center'>
+
+		</div>
+	</div>
 
 
-	h1{
-      font-family: Hamston;
-	}
 
-</style>
+	<style>
+
+			.title-btn{
+					background: var(--color-subtitle);
+			}
+
+		.header-landing{
+				background: var(--color-head);
+				font-family: barlow-sb;
+		}
+		.clear{
+        background: var(--color-head);
+		}
+
+		h1{
+				font-family: barlow-eb;
+				font-size: 9vw;
+        line-height: 0.9;
+		}
+      h2{
+          font-family: barlow-sb;
+          font-size: 5vw;
+          line-height: 0.9;
+      }
+
+      h3{
+          font-family: barlow-eb;
+          font-size: 6vw;
+          line-height: 0.9;
+					color: var(--color-subtitle);
+      }
+
+		.content-landing{
+				background: var(--color-background);
+				height: 35rem;
+		}
+
+
+	</style>
