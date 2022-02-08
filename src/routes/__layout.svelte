caeb7d54fc374ce3fb7598f983a86146d5af58cb
GuillianV <guillianvibert90@gmail.com>
2022-02-08 09:41:21 +0100

    setup project

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Nav from '../components/nav.svelte';
+</script>
+
+<Nav/>
+<!-- Slot représente le reste du code chragé par la page -->
+<slot></slot>
+
+<style>
+		/* Import Tailwind */
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
