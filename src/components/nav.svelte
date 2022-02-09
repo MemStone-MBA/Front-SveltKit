982a1e140b5a69cfdcb538646b9573b3c1e738e4
kirtsu04 <lucas.ru74@gmail.com>
2022-02-09 10:47:36 +0100

    Menu Page

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,6 +1,6 @@
-<nav>
-	<a href='/'>Home</a>
-	<a href='/landing'>Landing</a>
-	<a href='/socket'>Socket</a>
-	<a href="/bdd">Tables</a>
+<nav class="flex flex-col">
+	<a href='/' class="text-center">Home</a>
+	<a href='/landing' class="text-center">Landing</a>
+	<a href='/socket' class="text-center">Socket</a>
+	<a href="/bdd" class="text-center">Tables</a>
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,61 @@
-<h1>Welcome to SvelteKit</h1>
-<p  class='w-full'>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<link rel='stylesheet' href='static/css/index.css'>
+<script>
+	import Nav from '../components/nav.svelte';
+</script>
+
+
+<div class="flex flex-row backgroundsize">
+    <div class="colorbackmenu w-3/4 flex flex-col ">
+        <div class='content-center w-1/4 mx-auto mt-6'>
+            <img src='static/assets/memstone_logo.png' class='img-contain'>
+        </div>
+        <div class="flex flex-1 flex-col justify-between flexBetween">
+            <div class="m-8 ml-16 flex flex-row">
+                <div class="profilImage">
+    
+                </div>
+                <div class="flex flex-col ml-8 mr-32 boxName">
+                    <div class="flex flex-row justify-between">
+                        <div class="profilName ">
+                            JustinBridou
+                        </div>
+                        <div class="lvlExp">
+                            lvl 25
+                        </div>
+                    </div>
+                    <div class="bigExp mt-8">
+                        <div class="littleExp">
+    
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex flex-row footer px-4">
+                <div class="flex-1 flex flex-row items-center justify-center">
+                    <div class="buttonCollection mr-4">
+                        Collection
+                    </div>
+                    <div>
+                        <img src="static/assets/deck.svg">
+                    </div>
+                </div>
+                <div class="buttonFight p-8 flex-1">
+                    Combat
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="colorbackfriend w-1/4 text-2xl friend">
+        <h1 class="justify-content p-6 pl-0 ml-8 uppercase titlefriend">
+            Amis
+        </h1>
+        <div class="listfriend m-6">
+            <div class="onefriend m-4 p-4 text-black">
+                <div class="pl-4">
+                    Guillian
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
