cceeb4e19be7bbc425f2fc618e4ae2d3ca4be080
lucas-ru <lucas.ru74@gmail.com>
2022-02-15 18:57:22 +0100

    before pull

---
+++ src/routes/fight.svelte
@@ -0,0 +1,14 @@
+<link rel='stylesheet' href='static/css/fight.css'>
+<div class="flex flex-row">
+    <div class="flex flex-col">
+        <div class="flex flex-row">
+
+        </div>
+        <div>
+            
+        </div>
+        <div class="flex flex-row">
+
+        </div>
+    </div>
+</div>
