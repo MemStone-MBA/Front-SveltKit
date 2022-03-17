d944d012dfdf8d6e1c61d952ef0dc24d8afd6d67
lucas-ru <lucas.ru74@gmail.com>
2022-03-17 09:21:57 +0100

    add shop template

---
+++ src/routes/shop.svelte
@@ -0,0 +1,129 @@
+<link rel='stylesheet' href='static/css/shop.css'>
+<div class="backgroundsize colorbackmenu flex flex-row">
+    <div class="main_container">
+        <div class="title_journeyCard mt-8">
+            Carte à la une
+        </div>
+        <div class="flex flex-row w-full h-1/2 mt-8 h-auto">
+            <div class="flex-1">
+                <img src='static/assets/card_front.png' class='journeyCard'>
+                <div class="price mt-2">
+                    <div class="textprice">
+                        600
+                    </div>
+                    <img src='static/assets/coin.svg' class='coin mt-1'>
+                </div>
+            </div>
+            <div class="flex-1">
+                <img src='static/assets/card_front.png' class='journeyCard'>
+                <div class="price mt-2">
+                    <div class="textprice">
+                        600
+                    </div>
+                    <img src='static/assets/coin.svg' class='coin mt-1'>
+                </div>
+            </div>
+            <div class="flex-1">
+                <img src='static/assets/card_front.png' class='journeyCard'>
+                <div class="price mt-2">
+                    <div class="textprice">
+                        600
+                    </div>
+                    <img src='static/assets/coin.svg' class='coin mt-1'>
+                </div>
+            </div>
+        </div>
+        <div class="title_journeyPack">
+            Packs
+        </div>
+        <div class="flex flex-row w-full h-1/2 mt-8 h-auto">
+            <div class="flex-1">
+                <img src='static/assets/icon_pack.svg' class='journeyPack'>
+                <div class="price mt-2">
+                    <div class="flex flex-col">
+                        <div class="textprice">
+                        Pack 10 cartes
+                        </div>
+                        <div class="price">
+                            <div class="textprice">
+                             4000
+                            </div>
+                            <img src='static/assets/coin.svg' class='coin mt-1'>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-1">
+                <img src='static/assets/icon_pack.svg' class='journeyPack'>
+                <div class="price mt-2">
+                    <div class="flex flex-col">
+                        <div class="textprice">
+                        Pack 20 cartes
+                        </div>
+                        <div class="price">
+                            <div class="textprice">
+                             7000
+                            </div>
+                            <img src='static/assets/coin.svg' class='coin mt-1'>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex-1">
+                <img src='static/assets/icon_pack.svg' class='journeyPack'>
+                <div class="price mt-2">
+                    <div class="flex flex-col">
+                        <div class="textprice">
+                        Pack 50 cartes
+                        </div>
+                        <div class="price">
+                            <div class="textprice">
+                             22000
+                            </div>
+                            <img src='static/assets/coin.svg' class='coin mt-1'>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex-1 p-4 flex-col">
+        <div class="playerMoney flex flex-row">
+            200
+            <img src='static/assets/coin.svg' class='Pcoin mt-4 ml-4 mr-4'>
+        </div>
+        <div class="flex h-1/5 flex-col mb-4 bottom-0">
+            <div class="ourMoney flex flex-row  w-full">
+                1200
+                <img src='static/assets/coin.svg' class='coin mt-1'>
+
+            </div>
+            <div class="flex-1 euroMoney flex flex-row w-full">
+                9.99€
+            </div>
+        </div>
+        <div class="flex h-1/5 flex-col mb-4 transactioncontainere">
+            <div class="ourMoney flex flex-row  w-full">
+                4000
+                <img src='static/assets/coin.svg' class='coin mt-1'>
+
+            </div>
+            <div class="flex-1 euroMoney flex flex-row w-full">
+                29.99€
+            </div>
+        </div>
+        <div class="flex h-1/5 flex-col mb-4">
+            <div class="ourMoney flex flex-row  w-full">
+                15000
+                <img src='static/assets/coin.svg' class='coin mt-1'>
+
+            </div>
+            <div class="flex-1 euroMoney flex flex-row w-full">
+                99.99€
+            </div>
+        </div>
+        <div class="ButtonRetour mt-24">
+            Retour
+        </div>
+    </div>
+</div>
