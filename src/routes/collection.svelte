5ce419a999326cdf34104e12ace4e0b26d9779e7
kirtsu04 <lucas.ru74@gmail.com>
2022-03-22 13:02:09 +0100

    create deck

--- src/routes/collection.svelte
+++ src/routes/collection.svelte
@@ -11,6 +11,9 @@
     var cardsTitle = "Cards "
     var cardsId =[]
     var cards = []
+    var deck = []
+
+    var IndexDeck = 0
 
     onMount(() => {
 
@@ -24,6 +27,7 @@
           }
           cardsTitle = "Cards (" + res.length + ")"
           cards = res
+          console.log(cards)
         })
 
 
@@ -37,7 +41,24 @@
     });
 
 
+    function HandleAddCard(img){
+      console.log(deck)
+      document.querySelector("."+img.name).classList.add("disableElement")
+      deck[IndexDeck] = img
+      IndexDeck++
+    }
 
+    function HandleDeleteCard(img){
+      for(var i=0;i<deck.length;i++){
+        if(deck[i].id === img.id){
+          deck.splice(i,1)
+          console.log(document.querySelector("."+img.name))
+          document.querySelector("."+img.name).classList.remove("disableElement")
+          IndexDeck--
+        }
+      }
+      deck = deck
+    }
 
     function goToMenu() {
         goto('/')
@@ -53,7 +74,15 @@
     <div class="colorbackmenu w-full flex flex-row ">
        <div class="colorbackfriend w-1/4 mr-10 flex flex-col">
             <div class="text-center titleCollection m-2">Deck</div>
-            <div class="gray_bg_custom flex-1 overflow-y-scroll">container cards</div>
+            <div class="gray_bg_custom flex-1 overflow-y-scroll">
+              {#each deck as element}
+              <div class="flex flex-row justify-between containercard my-2">
+                <img src="http://51.210.104.99:8001/getImage/{element.path}" class="backgroundimage">
+                <div class="deckcard ml-4">{element.name}</div>
+                <div class="unselectcard px-4 py-2" on:dblclick={()=>{HandleDeleteCard(element)}}>X</div>
+              </div>
+                {/each}
+            </div>
             <div class="flex justify-center">
                 <div on:click={goToMenu} class="buttonSave buttonDetail p-4 m-6">Retour</div>
             </div>
@@ -65,7 +94,7 @@
                 
                 
                 {#each cards as img}
-                    <div><img src="http://51.210.104.99:8001/getImage/{img.path}" class="p-2"></div>
+                    <div><img src="http://51.210.104.99:8001/getImage/{img.path}" class="p-2 {img.name}" on:dblclick={()=>{HandleAddCard(img)}}></div>
                 {/each}
 
             </div>
