b5471d839d80ad73eab577f70504bf5354271e7c
lsmnt <leasimonet56@gmail.com>
2022-03-26 21:19:16 +0100

    change friend style

--- src/components/friendmenu.svelte
+++ src/components/friendmenu.svelte
@@ -2,13 +2,25 @@
     import { onMount } from 'svelte';
 
     onMount(() => {
-        document.querySelector('.closeFriendMenu').addEventListener('click', () => {
+       /* document.querySelector('.closeFriendMenu').addEventListener('click', () => {
             document.querySelector('.friend').classList.toggle('closeFriendMenuTransition')
-        })
+        })*/
     })
+
+    export function ToggleMenu(){
+
+        let friendMenu = document.querySelector('.friend')
+        console.log(friendMenu)
+        if (friendMenu.classList.contains("friend-open"))
+            friendMenu.classList.remove("friend-open")
+        else
+            friendMenu.classList.add("friend-open")
+    }
+
+
 </script>
 
-<div class="colorbackfriend w-1/4 text-2xl friend">
+<div class="colorbackfriend w-1/4 text-2xl friend h-full z-40">
     <h1 class="justify-content p-6 pl-0 ml-8 uppercase titlefriend">
         Amis
     </h1>
@@ -24,19 +36,26 @@
             </div>  
         </div>
     </div>
-    <div class="closeFriendMenu">X</div>
+    <div class="closeFriendMenu" on:click={()=>{ToggleMenu()}}>X</div>
 </div>
 
 <style>
+    .friend{
+        position: fixed;
+        right: 0;
+        top:0;
+        transition: 0.5s all ease;
+        transform: translateX(100%);
+
+    }
+
+
     .closeFriendMenu {
         position: absolute;
         background-color: red;
         transition: 1s all ease-in-out;
     }
 
-    .closeFriendMenuTransition {
-        transform: translate(100%) !important;
-    }
 </style>
 
 
